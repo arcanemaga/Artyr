43e2aa46340600fcc248cda5c51ac97a8692780b
arcanemaga <vadik.aladik5@gmail.com>
2019-03-27 03:08:21 +0300

    My first initial commit =)

---
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am Artyr. And I would like to know about git more!");
+            Console.ReadLine();
+        }
+    }
+}
